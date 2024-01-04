15c2786e39bad954801ff045d1667e5847041751
Asphode1 <maitrungkien1910@gmail.com>
2024-01-05 00:35:09 +0700

    change vite base name

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: '/IoT/',
+	base: '/143804_20231_Frontend/',
 	server: {
 		proxy: {
 			'/api': {
