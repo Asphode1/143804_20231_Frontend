1513be8746a48e0e12f1ad87f831f3d4270050cd
Asphode1 <maitrungkien1910@gmail.com>
2024-01-05 10:00:00 +0700

    fix reload

--- src/components/pages/main/index.tsx
+++ src/components/pages/main/index.tsx
@@ -1,23 +1,33 @@
 import { useEffect, useState } from 'react'
 import Metrics from './metrics'
-import { Device } from '../../../types'
+import { Device, House } from '../../../types'
 import { DeviceType } from '../../../types/enums'
 import LED from './led'
 import Fire from './fire'
 import useHouse from '../../../hooks/useHouse'
 import { useLocation } from 'react-router-dom'
+import axios from '../../../shared/axios'
 
 export default function Main() {
 	const [devices, setDevices] = useState<Device[]>([])
 
-	const { house } = useHouse()
+	const { house, setHouse } = useHouse()
 	const location = useLocation()
+
+	useEffect(() => {
+		if (location.pathname === '/') {
+			axios
+				.get('/api/house/get')
+				.then((res) => res.data as House[])
+				.then((res) => setHouse((house) => res.find((h) => h.id === house?.id) ?? house))
+		}
+	}, [location]) // eslint-disable-line react-hooks/exhaustive-deps
 
 	useEffect(() => {
 		if (house) {
 			setDevices(house.devices)
 		}
-	}, [house, location])
+	}, [house])
 	return (
 		<>
 			<Metrics devices={devices.filter((device) => device.type === DeviceType.SENSOR)} />
